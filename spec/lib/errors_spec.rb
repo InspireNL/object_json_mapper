1ba01442cbed3f999511cf5f0696ba8170df4d94
droptheplot <novikov359@gmail.com>
2017-02-22 16:41:02 +0100

    Add support for old and new rails errors format

---
+++ spec/lib/errors_spec.rb
@@ -0,0 +1,48 @@
+describe ActiveUMS::Errors do
+  let(:user) { User.new }
+
+  context 'using rails 4.2 validation errors' do
+    describe '#load_errors' do
+      let(:errors) do
+        {
+          email: ["can't be blank"],
+          password: ["is too short", "is too long"]
+        }
+      end
+
+      it 'correctly loads errors' do
+        user.load_errors(errors)
+
+        expect(user.errors.full_messages).to match_array([
+          "Email can't be blank",
+          "Password is too short",
+          "Password is too long"
+        ])
+      end
+    end
+  end
+
+  context 'using rails 5.0 validation errors' do
+    describe '#load_errors' do
+      let(:errors) do
+        {
+          email: [{ error: :blank }],
+          password: [
+            { error: :too_short, count: 5 },
+            { error: :too_long, count: 10 }
+          ]
+        }
+      end
+
+      it 'correctly loads errors' do
+        user.load_errors(errors)
+
+        expect(user.errors.full_messages).to match_array([
+          "Email can't be blank",
+          "Password is too short (minimum is 5 characters)",
+          "Password is too long (maximum is 10 characters)"
+        ])
+      end
+    end
+  end
+end
