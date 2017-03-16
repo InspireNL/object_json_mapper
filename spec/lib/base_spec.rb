038eaadb207671da8e1f0f6e2153f508db825a98
droptheplot <novikov359@gmail.com>
2017-03-16 11:03:41 +0100

    Fix `Base.create` specs

--- spec/lib/base_spec.rb
+++ spec/lib/base_spec.rb
@@ -21,14 +21,31 @@ describe ActiveUMS::Base do
   end
 
   describe '.create' do
-    let!(:query) do
-      stub_request(:post, 'http://localhost:3000/users')
-        .to_return(body: { id: 1 }.to_json)
+    context 'without errors' do
+      let!(:query) do
+        stub_request(:post, 'http://localhost:3000/users')
+          .to_return(body: { id: 1 }.to_json)
+      end
+
+      it 'creates record' do
+        expect(User.create(id: 1)).to eq(User.persist(id: 1))
+        expect(query).to have_been_requested
+      end
     end
 
-    it 'creates record' do
-      expect(User.create(id: 1)).to eq(User.persist(id: 1))
-      expect(query).to have_been_requested
+    context 'with errors' do
+      let!(:query) do
+        stub_request(:post, 'http://localhost:3000/users')
+          .to_return(status: 422)
+      end
+
+      it 'returns new record' do
+        user = User.create
+
+        expect(user).to be_a(User)
+        expect(user.persisted?).to be false
+        expect(query).to have_been_requested
+      end
     end
   end
 
